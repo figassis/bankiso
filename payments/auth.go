a0e0fcc0d2f8c8ff5e28874faddd32732c6c95ca
Assis Ngolo <figassis@nellcorp.com>
2016-08-21 17:26:23 +0100

    added missing schema

--- payments/auth.go
+++ payments/auth.go
@@ -10,14 +10,16 @@ import (
 )
 
 /*
-Implementation of the ISO20022 standard for Authorities and Financial Investigations
-https://www.iso20022.org/payments_messages.page - auth
+ISO 20022 Implementation
+Business Domain: Payments
+Message Set: Authorities and Financial Investigations (auth)
+Message Definitions: https://www.iso20022.org/payments_messages.page
+Message Schemas: https://github.com/figassis/iso20022/tree/master/msg-schemas/payments/auth
 
-Authorities (auth) transactions are as follows:
-1  -  InformationRequestOpeningV01
-
-2  -  InformationRequestResponseV01
-3  -  InformationRequestStatusChangeNotificationV01
+Messages
+1.	InformationRequestOpeningV01 - auth.001.001.01
+2.	InformationRequestResponseV01 - auth.002.001.01
+3.	InformationRequestStatusChangeNotificationV01 - auth.003.001.01
 */
 
 type AccountHolder struct {
